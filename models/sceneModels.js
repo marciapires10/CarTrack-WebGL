6e0138eed8db09e97fcc4fa2ab00ead9f05d9434
tomasfilipe7 <tomasfilipe7@ua.pt>
2020-12-14 22:19:41 +0000

    Entire new project welelelelelel

---
+++ models/sceneModels.js
@@ -0,0 +1,82 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  For instantiating the scene models.
+//
+//  J. Madeira - November 2018
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------------
+//
+//  Constructors
+//
+
+
+function emptyModelFeatures() {
+
+	// EMPTY MODEL
+	this.name = "";
+
+	this.vertices = [];
+
+	this.normals = [];
+	
+	this.colors = [];
+	// Transformation parameters
+
+	// Displacement vector
+	
+	this.tx = 0.5;
+	
+	this.ty = 0.5;
+	
+	this.tz = 0.0;	
+	
+	// Rotation angles	
+	
+	this.rotAngleXX = 0.0;
+	
+	this.rotAngleYY = 0.0;
+	
+	this.rotAngleZZ = 0.0;	
+
+	// Scaling factors
+	
+	this.sx = 0.25;
+	
+	this.sy = 0.25;
+	
+	this.sz = 0.25;		
+	
+	// Animation controls
+	
+	this.rotXXOn = true;
+	
+	this.rotYYOn = true;
+	
+	this.rotZZOn = true;
+	
+	this.rotXXSpeed = 1.0;
+	
+	this.rotYYSpeed = 1.0;
+	
+	this.rotZZSpeed = 1.0;
+	
+	this.rotXXDir = 1;
+	
+	this.rotYYDir = 1;
+	
+	this.rotZZDir = 1;
+	
+	// Material features
+	
+	this.kAmbi = [ 0.2, 0.2, 0.2 ];
+	
+	this.kDiff = [ 0.7, 0.7, 0.7 ];
+
+	this.kSpec = [ 0.7, 0.7, 0.7 ];
+
+	this.nPhong = 100;
+}
+
+
