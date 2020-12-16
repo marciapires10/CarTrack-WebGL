7b553e54209d2e985dd9c64a94c067c7385cc780
marciapires <marcia.pires19@live.com>
2020-12-16 15:25:43 +0000

    More html+js changes

--- carMovement.js
+++ carMovement.js
@@ -1,6 +1,8 @@
-var car_speed = 1.0;
-
+var car_speed = 0.0;
+var prev_car_speed = 0.0;
 var car_middle_point = [4.5, 0.0, 4.5];
+
+
 
 function moveCar(road)
 {
@@ -75,11 +77,19 @@ function FlipCar(prev_dir, new_dir)
 function increase_car_speed()
 {
 	// Aumentar velocidade
+	if(car_speed != 4){
+		car_speed += 0.25;
+	}
+	return car_speed;
 }
 
 function decrease_car_speed()
 {
 	// Diminuir velocidade
+	if(car_speed != 0.25){
+		car_speed -= 0.25;
+	}
+	return car_speed;
 }
 
 function reset_car()
@@ -89,4 +99,26 @@ function reset_car()
 	sceneModels[1].ty = car_starting_pos[1];
 	sceneModels[1].tz = car_starting_pos[2];
 	sceneModels[1].rotAngleYY = 0;
+	current_road.reset_road();
+	car_speed = 0.0;
+}
+
+function start_race()
+{
+	car_middle_point = [4.5, 0.0, 4.5];
+	sceneModels[1].tx = car_starting_pos[0];
+	sceneModels[1].ty = car_starting_pos[1];
+	sceneModels[1].tz = car_starting_pos[2];
+	sceneModels[1].rotAngleYY = 0;
+	current_road.reset_road();
+	car_speed = 1.0;
+}
+
+function pause_race(){
+	prev_car_speed = car_speed;
+	car_speed = 0.0;
+}
+
+function cont_race(){
+	car_speed = prev_car_speed;
 }
